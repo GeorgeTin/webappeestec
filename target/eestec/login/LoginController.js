211f86d0b51153d4a19fcb0a42209a63491ab698
George Tintareanu <george.tintareanu@devpadawan.ro>
2016-11-07 17:38:15 +0200

    Login screen

---
+++ target/eestec/login/LoginController.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Aimandis on 11/7/2016.
+ */
+var myApp = angular.module('myApp',[]);
+
+myApp.controller('LoginController', ['$scope', , function($scope) {
+    $scope.login = function(){
+        if($scope.username === "username" && $scope.password === "parola");
+    }
+}]);
